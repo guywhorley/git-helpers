d61cdae89fe49a65651b3ccb3ff39c7505fa71d4
Guy Whorley <guy.whorley@gmail.com>
2016-06-16 16:55:21 -0700

    fixing notes

--- gitdo.cmd
+++ gitdo.cmd
@@ -1,13 +1,15 @@
 @echo off
-rem  git add, git commit, git push in one step
+rem  git add, git commit, git push to origin in one step
 rem  Only happy path is considered. Contains no error checking.
 rem
-rem   Syntax: gadd %1 %2 %3 %4
+rem  Note: a remote called 'origin' must exist in the repository.
+rem
+rem   Syntax: gadd %1 %2 %3
 rem
 rem   %1 - file to add, "*" or "./someFile.txt"
 rem   %2 - commit message, enclosed in double quotes
-rem   %3 - remote repo
-rem  %4 - branch
+rem   %3 - branch
+
 
 rem add to source control
 cls
@@ -18,5 +20,5 @@ rem commit
 git status
 git commit -m %2
 
-rem push to remote 
-git push -u %3 %4
+rem push to remote
+git push -u origin %3
