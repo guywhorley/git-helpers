32bb81258342c3a33a452ccdd33f8622e3d72c8c
Guy Whorley <guy.whorley@gmail.com>
2016-06-17 14:18:26 -0700

    added parm defaults and checking

--- gitdo.cmd
+++ gitdo.cmd
@@ -9,16 +9,37 @@ rem
 rem   %1 - file to add, "*" or "./someFile.txt"
 rem   %2 - commit message, enclosed in double quotes
 rem   %3 - branch
+rem   %4 - remote
 
-
-rem add to source control
 cls
-git status
-git add %1
+SETLOCAL
 
-rem commit
-git status
-git commit -m %2
+rem SET DEFAULTS
+rem   Note that there must be no spaces between values
+rem   and the equals signs... yep, it's a cmds shell thing!
+SET BRANCH="master"
+SET REMOTE="origin"
+SET MESSAGE="Saving latest work."
+SET FILENAME="-A"
 
-rem push to remote
-git push -u origin %3
+rem echo %BRANCH% %REMOTE% %MESSAGE% %FILENAME%
+IF not "%~1" == "" ( SET FILENAME=%~1  )
+IF not "%~2" == "" ( SET MESSAGE="%~2" )
+IF not "%~3" == "" ( SET BRANCH=%~3  )
+IF not "%~4" == "" ( SET REMOTE=%~4  )
+
+rem Remove double quotes around BRANCH and REMOTE
+SET BRANCH=%BRANCH:"=%
+SET REMOTE=%REMOTE:"=%
+
+rem ADD FILES TO SOURCE CONTROL
+git status
+git add %FILENAME%
+
+rem COMMIT TO LOCAL STAGING AREA
+git commit -m %MESSAGE%
+
+rem PUSH TO REMOTE
+git push -u %REMOTE% %BRANCH%
+
+EndLocal
